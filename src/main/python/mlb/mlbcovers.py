a2d0916d820cdd827ac1e9ebfa08c13fc0a4c2de
dj <darjens@gmail.com>
2024-04-30 19:38:09 -0400

    4-30 update

--- src/main/python/mlb/mlbcovers.py
+++ src/main/python/mlb/mlbcovers.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import json
 import requests
@@ -11,24 +12,27 @@ url = 'https://www.covers.com/sports/mlb/matchups'
 page = requests.get(url)
 soup = soup(page.content, 'html.parser')
 matchup_pattern = re.compile("mlb-matchup-link")
-pitcher_pattern = re.compile("starter-table")
 matchups = soup.find_all('a', {'data-linkcont': matchup_pattern})
 print('matchups - ')
 print(len(matchups))
 data = {}
-
+# driver = webdriver.Chrome()
 for matchup in matchups:
     href = matchup.attrs.get('href')
     matchup_url = 'https://www.covers.com'+href
     print(matchup_url)
-    driver = webdriver.Chrome()
-    driver.get(matchup_url)
-    matchup_html = driver.page_source
-    matchup_soup = soup(matchup_html, 'html.parser')
-    pitcher_table = matchup_soup.find_all('table', attrs={'class':"starter-table"})
-    rows = pitcher_table.find_all('tr')
-    print('pitchers - ')
-    print(len(pitcher_table))
+    page = requests.get(url)
+    # driver.get(matchup_url)
+    time.sleep(5)
+    # driver.execute_script("window.scrollTo(0, 1000);")
+    time.sleep(5)
+    matchup_soup = soup(page.content, 'html.parser')    
+    pitchers = soup.find_all('a', {'class':"anchor-with-border"})
+    matchup = pd.read_html(matchup_url)
+    away_pitcher_last5 = matchup[11]
+    print(away_pitcher_last5)
+    home_pitcher_last5 = matchup[13]
+    print(home_pitcher_last5)
     time.sleep(8)
     
 
