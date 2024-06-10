9869d4d0a94032d41484f3b9cb3f876181b204a8
dj <darjens@gmail.com>
2024-06-10 18:11:22 -0400

    6-10-2

--- src/main/python/mlb/mlb.py
+++ src/main/python/mlb/mlb.py
@@ -1,12 +1,20 @@
-from mlbespn import todays_games
+# from mlbespn import todays_games
 from bs4 import BeautifulSoup as soup
 import requests
+import re
 
-espn_games = todays_games()
+# espn_games = todays_games()
 url = 'https://www.covers.com/sports/mlb/matchups'
 page = requests.get(url)
 soup = soup(page.content, 'html.parser')
 matchup_sections = soup.find_all('div',{'class': 'cmg_matchup_game_box cmg_game_data'})
 
 for matchup_section in matchup_sections:
+    matchup_pattern = re.compile("mlb-matchup-link")
+    matchup_a = matchup_section.find('a', {'data-linkcont': matchup_pattern})
+    matchup_href = matchup_a.attrs.get('href')
+    print(matchup_href)
+    matchup_pattern = re.compile("mlb/teams/main")
+    matchup_teams = matchup_section.find_all('a', {'href': matchup_pattern})
+    print(matchup_teams)
     
